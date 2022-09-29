3d9150e48d8e3d2eb4d685577713375eeb5cb089
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2022-09-29 20:55:22 -0300

    Adicionando lógicas iniciais

--- src/pages/Question/index.jsx
+++ src/pages/Question/index.jsx
@@ -1,16 +1,43 @@
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import React from "react";
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import QuizzesDB from "@/db/Quizzes.json";
 
 const Question = () => {
-  const { name } = useSelector((state) => state.user);
   const { id } = useParams();
+  const { Quizzes } = QuizzesDB;
+  const selectedQuiz = Quizzes.find(({ questions }) =>
+    questions.find((question) => question.idQuestion === id)
+  );
+  const selectedQuestion = selectedQuiz.questions.find(
+    (question) => question.idQuestion === id
+  );
+
+  const indexOfSelectedQuestion = selectedQuiz.questions
+    .map((e) => JSON.stringify(e))
+    .indexOf(JSON.stringify(selectedQuestion));
+
+  // TODO: verificar que quando for a última posição do array, o próximo deve enviar para a posição 0 (o inverso acontece quando for o botão de voltar)
+  const nextQuestionId =
+    selectedQuiz.questions[indexOfSelectedQuestion + 1]?.idQuestion;
 
   return (
     <Typography>
       <div>Página de questão, aqui poderá ser respondida uma questão</div>
-      {JSON.stringify({ name, id }, null, 2)}
+      <pre>
+        {JSON.stringify(
+          { indexOfSelectedQuestion, nextQuestionId, selectedQuestion },
+          null,
+          2
+        )}
+      </pre>
+      <Button
+        component={Link}
+        to={`/questao/${nextQuestionId}`}
+        variant="contained"
+      >
+        Próxima Questão
+      </Button>
     </Typography>
   );
 };
--- src/pages/Score/index.jsx
+++ src/pages/Score/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Score = () => {
+  // TODO: Criar método que retorne a pontuação do usuário
   return (
     <div>Página em que o usuário poderá ver a sua pontuação das questões</div>
   );
