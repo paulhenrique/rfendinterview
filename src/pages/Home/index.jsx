dba2a86567c38f9ae61cc8bec1cfcc204183e764
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2022-10-04 19:08:47 -0300

    Ajustando home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,29 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import Quizzes from "@/db/Quizzes.json";
+import { Button, Box } from "@mui/material";
+import { Link } from "react-router-dom";
 
+/**
+ * Onde são listados os Quizzes
+ * @returns
+ */
 const Home = () => {
-  return <Typography>Home da aplicação</Typography>;
+  // TODO: Criar método que retorne os quizzes e coloque na tela
+  return (
+    <>
+      <Box display="flex" flexDirection="column" gap="20px">
+        {Quizzes.Quizzes.map(({ title, questions }) => (
+          <Button
+            component={Link}
+            to={`/questao/${questions[0]?.idQuestion}`}
+            variant="contained"
+          >
+            {title}
+          </Button>
+        ))}
+      </Box>
+    </>
+  );
 };
 
 export default Home;
