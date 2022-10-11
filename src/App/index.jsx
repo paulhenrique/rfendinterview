26a7a933106078feade59bd93422b5184b99f231
Paulo Henrique <phvcandido@gmail.com>
2022-10-11 19:55:30 -0300

    Ajustes de layout

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -3,7 +3,7 @@ import Router from "@/router";
 import { SnackbarProvider } from "notistack";
 import { Provider } from "react-redux";
 import { store } from "@/store";
-import { Container, CssBaseline } from "@mui/material";
+import { Box, CssBaseline } from "@mui/material";
 import AppBar from "@/components/AppBar";
 
 const App = () => {
@@ -11,10 +11,19 @@ const App = () => {
     <SnackbarProvider>
       <Provider store={store}>
         <CssBaseline />
-        <Container>
+        <Box
+          sx={{
+            maxWidth: {
+              md: "50%",
+              sm: "70%",
+              xs: "90%",
+            },
+            mx: "auto",
+          }}
+        >
           <AppBar />
           <Router />
-        </Container>
+        </Box>
       </Provider>
     </SnackbarProvider>
   );
