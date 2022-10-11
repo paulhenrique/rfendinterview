e3231a47dbac31fdf164d49aa064a50abc0b0eb0
Paulo Henrique <phvcandido@gmail.com>
2022-10-11 19:01:22 -0300

    Adiciona página notFound

--- src/pages/Question/index.jsx
+++ src/pages/Question/index.jsx
@@ -2,24 +2,36 @@ import { Button, Typography } from "@mui/material";
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 import QuizzesDB from "@/db/Quizzes.json";
+import NotFound from "@/pages/NotFound";
 
 const Question = () => {
   const { id } = useParams();
   const { Quizzes } = QuizzesDB;
-  const selectedQuiz = Quizzes.find(({ questions }) =>
+  const selectedQuiz = Quizzes?.find(({ questions }) =>
     questions.find((question) => question.idQuestion === id)
   );
-  const selectedQuestion = selectedQuiz.questions.find(
+
+  const goToNotFound = () => <NotFound />;
+
+  if (!id || !selectedQuiz) {
+    return goToNotFound();
+  }
+
+  const selectedQuestion = selectedQuiz?.questions.find(
     (question) => question.idQuestion === id
   );
 
-  const indexOfSelectedQuestion = selectedQuiz.questions
-    .map((e) => JSON.stringify(e))
-    .indexOf(JSON.stringify(selectedQuestion));
+  const indexOfSelectedQuestion = selectedQuiz.questions.findIndex(
+    (question) => question.idQuestion === id
+  );
 
   // TODO: verificar que quando for a última posição do array, o próximo deve enviar para a posição 0 (o inverso acontece quando for o botão de voltar)
   const nextQuestionId =
     selectedQuiz.questions[indexOfSelectedQuestion + 1]?.idQuestion;
+
+  if (!selectedQuestion) {
+    return goToNotFound();
+  }
 
   return (
     <Typography>
