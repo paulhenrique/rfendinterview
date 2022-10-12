efafb34b870b04dd1b57658deb3c47da8ed4afce
Paulo Henrique <phvcandido@gmail.com>
2022-10-12 00:58:46 -0300

    Implementa persistencia do redux

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -5,25 +5,31 @@ import { Provider } from "react-redux";
 import { store } from "@/store";
 import { Box, CssBaseline } from "@mui/material";
 import AppBar from "@/components/AppBar";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+const persistor = persistStore(store);
 
 const App = () => {
   return (
     <SnackbarProvider>
       <Provider store={store}>
-        <CssBaseline />
-        <Box
-          sx={{
-            maxWidth: {
-              md: "50%",
-              sm: "70%",
-              xs: "90%",
-            },
-            pt: "104px",
-            mx: "auto",
-          }}
-        >
-          <Router excedent={<AppBar />} />
-        </Box>
+        <PersistGate persistor={persistor} loading={null}>
+          <CssBaseline />
+          <Box
+            sx={{
+              maxWidth: {
+                md: "50%",
+                sm: "70%",
+                xs: "90%",
+              },
+              pt: "104px",
+              mx: "auto",
+            }}
+          >
+            <Router excedent={<AppBar />} />
+          </Box>
+        </PersistGate>
       </Provider>
     </SnackbarProvider>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import user from "@/store/features/user";
 import quiz from "@/store/features/quiz";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  user,
+  quiz,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user,
-    quiz,
-  },
+  reducer: persistedReducer,
 });
