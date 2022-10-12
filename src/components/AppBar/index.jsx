c8cfc0db8497a929397c790810dea9733517edb7
Paulo Henrique <phvcandido@gmail.com>
2022-10-12 10:02:48 -0300

    Ajusta exibição do botão home

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -10,11 +10,12 @@ import {
   Typography,
 } from "@mui/material";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Home } from "@mui/icons-material";
 import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 import ContentDrawer from "./components/ContentDrawer";
 import { ToolbarStyle } from "./style";
+
 const AppBarNoMui = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
 
@@ -23,23 +24,28 @@ const AppBarNoMui = () => {
   };
 
   const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
-
+  const { pathname } = useLocation();
   return (
     <>
       <AppBar position="fixed">
         <Box display="flex" alignItems="center" justifyContent="space-between">
           <Toolbar sx={ToolbarStyle}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              sx={{ color: "white" }}
-              component={Link}
-              to="/"
-            >
-              <Home />
-            </IconButton>
+            {pathname !== "/" ? (
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                sx={{ color: "white" }}
+                component={Link}
+                to="/"
+              >
+                <Home />
+              </IconButton>
+            ) : (
+              <span></span>
+            )}
+
             <Tooltip title="Abrir Menu">
               <IconButton
                 size="large"
