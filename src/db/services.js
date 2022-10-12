56390a547e497d8fa835941e18cd84c8307491b3
Paulo Henrique <phvcandido@gmail.com>
2022-10-11 21:29:18 -0300

    Separa em services

---
+++ src/db/services.js
@@ -0,0 +1,20 @@
+import QuizzesDB from "@/db/Quizzes.json";
+
+export const getSelectedQuestion = (selectedQuiz, id) => [
+  selectedQuiz?.questions.find((question) => question.idQuestion === id),
+  selectedQuiz.questions.findIndex((question) => question.idQuestion === id),
+];
+
+export const getSelectedQuizByQuestionId = (id) => {
+  const { Quizzes } = QuizzesDB;
+  return Quizzes?.find(({ questions }) =>
+    questions.find((question) => question.idQuestion === id)
+  );
+};
+
+export const getQuestionId = (selectedQuiz) => (index) => {
+  if (index > selectedQuiz.questions.lenght) {
+    return getQuestionId(0);
+  }
+  return selectedQuiz.questions[index]?.idQuestion;
+};
