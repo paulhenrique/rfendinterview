5c407947b53387b071ed28a4cf41a807a0d26aa1
Paulo Henrique <phvcandido@gmail.com>
2022-10-12 10:17:13 -0300

    Adiciona botão de finalização ao quiz

--- src/pages/Question/components/EndButtons.jsx
+++ src/pages/Question/components/EndButtons.jsx
@@ -1,19 +1,29 @@
-import { Box, Button, Collapse } from "@mui/material";
+import { Box, Button, Collapse, Tooltip } from "@mui/material";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 export const EndButtons = () => {
-  const { currentQuestionIndex, questions } = useSelector(
+  const { currentQuestionIndex, questions, id } = useSelector(
     (state) => state.quiz
   );
+  const { realizedQuizes } = useSelector((state) => state.user);
   const left = currentQuestionIndex - 1;
   const right = currentQuestionIndex + 1;
   const previousQuestion = questions[left]?.idQuestion;
   const nextQuestion = questions[right]?.idQuestion;
 
+  const allQuestionsAnswered =
+    realizedQuizes.find((item) => id === item.idQuiz).answers.length ===
+    questions.length;
   return (
-    <Box display="flex" justifyContent="space-between">
-      <Collapse in={Boolean(previousQuestion)}>
+    <Box
+      sx={{
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr",
+        justifyItems: "end",
+      }}
+    >
+      <Collapse in={Boolean(previousQuestion)} sx={{ justifySelf: "start" }}>
         <Button
           component={Link}
           to={`/questao/${previousQuestion}`}
@@ -22,15 +32,32 @@ export const EndButtons = () => {
           Anterior
         </Button>
       </Collapse>
-      <Collapse in={Boolean(nextQuestion)}>
-        <Button
-          component={Link}
-          to={`/questao/${nextQuestion}`}
-          variant="contained"
-        >
-          Próxima
-        </Button>
-      </Collapse>
+      <Box>
+        <Collapse in={Boolean(nextQuestion)}>
+          <Button
+            component={Link}
+            to={`/questao/${nextQuestion}`}
+            variant="contained"
+          >
+            Próxima
+          </Button>
+        </Collapse>
+        <Collapse in={!nextQuestion}>
+          <Tooltip title="Você precisa responder todas as questões para finalizar o quiz">
+            <span>
+              <Button
+                color={"success"}
+                disabled={!allQuestionsAnswered}
+                component={Link}
+                to={`/historico/`}
+                variant="contained"
+              >
+                Finalizar
+              </Button>
+            </span>
+          </Tooltip>
+        </Collapse>
+      </Box>
     </Box>
   );
 };
