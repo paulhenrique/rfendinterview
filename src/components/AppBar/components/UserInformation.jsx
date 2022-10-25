9f49729d8c0ad6cbfa86d10ae7ba3ded8b7e3a70
Paulo Henrique <phvcandido@gmail.com>
2022-10-25 19:37:23 -0300

    Aplicando no menu a pontuação do usuário

--- src/components/AppBar/components/UserInformation.jsx
+++ src/components/AppBar/components/UserInformation.jsx
@@ -1,10 +1,17 @@
 import { Avatar, Box, Collapse, Typography } from "@mui/material";
 import { useSelector } from "react-redux";
+import { useScore } from "../../../hooks/useScore";
+import CircularProgressWithProgress from "./CircularProgressWithProgress";
 import { userInformationStyle } from "./userInformationStyle";
 
 export const UserInformation = () => {
   const { name = "" } = useSelector((state) => state.user);
-
+  const {
+    countHitsInPercent,
+    countRealizedQuizes,
+    correctAnswers,
+    passedQuestions,
+  } = useScore();
   return (
     <Collapse in={Boolean(name)}>
       <Box sx={userInformationStyle.box}>
@@ -12,13 +19,20 @@ export const UserInformation = () => {
           src={`https://robohash.org/${name}`}
           sx={userInformationStyle.avatar}
         />
-        <Box>
-          <Typography variant="h6" component="p">
-            {name}
-          </Typography>
-          <Typography variant="caption" component="p">
-            Pontuação: <b>- - </b>
-          </Typography>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Box>
+            <Typography variant="h6" component="p">
+              {name}
+            </Typography>
+            <Typography variant="caption" component="p" sx={{ mt: 0, pt: 0 }}>
+              Quizzes realizados: {countRealizedQuizes}; Acertos:{" "}
+              {countHitsInPercent}
+            </Typography>
+          </Box>
+          <CircularProgressWithProgress
+            score={correctAnswers}
+            total={passedQuestions}
+          />
         </Box>
       </Box>
     </Collapse>
