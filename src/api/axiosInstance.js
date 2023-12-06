c2f3a4ac946bf22a9c2b18f632fff27fa31a1ffa
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2023-12-06 19:02:48 -0300

    Atualiza endpoint

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -4,7 +4,8 @@ import axios from "axios";
  * Define uma instancia do axios com a baseURL definida
  */
 export const axiosInstance = axios.create({
-  baseURL: "https://interviewapipuc.azurewebsites.net/api",
+  // baseURL: "https://interviewapipuc.azurewebsites.net/api",
+  baseURL: "https://interviewapi2.azurewebsites.net/api",
 });
 
 export default axiosInstance;
